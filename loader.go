ca3f1902ae514f2121382b40e11023ad79f7feab
Will Faught <willfaught@users.noreply.github.com>
2020-04-30 12:02:24 -0700

    Use tools/go/packages

--- loader.go
+++ loader.go
@@ -1,201 +1,121 @@
 package turbine
 
 import (
+	"context"
+	"fmt"
 	"go/ast"
-	"go/build"
-	"go/importer"
-	"go/parser"
 	"go/token"
-	"go/types"
-	"os"
+	"strings"
 
-	"golang.org/x/tools/go/loader" // TODO: Use tools/.../go/packages
+	"golang.org/x/tools/go/packages"
 )
 
-// Load returns the package for the import path using BuildLoader.
-func Load(path string) (*Package, error) {
-	return BuildLoader.Load(path)
+// LoadPackage returns the package for importPath.
+func LoadPackage(importPath string) (*Package, error) {
+	return Loader{}.LoadPackage(importPath)
 }
 
-// LoadTest returns the test package for the import path using BuildLoader.
-func LoadTest(path string) (*Package, error) {
-	return BuildLoader.LoadTest(path)
+// LoadTestPackage returns the test package for importPath.
+func LoadTestPackage(importPath string) (*Package, error) {
+	return Loader{}.LoadTestPackage(importPath)
 }
 
-// LoadTestExternal returns the external test package for the import path using
-// BuildLoader.
-func LoadTestExternal(path string) (*Package, error) {
-	return BuildLoader.LoadTestExternal(path)
+// LoadExternalTestPackage returns the external test package for importPath.
+func LoadExternalTestPackage(importPath string) (*Package, error) {
+	return Loader{}.LoadExternalTestPackage(importPath)
 }
 
-// Loader loads build, node, token, and type information about packages.
+// Loader provides Packages for import paths.
 type Loader struct {
-	BuildContext    build.Context    // The build context.
-	BuildImportMode build.ImportMode // The build import mode.
-	ParserMode      parser.Mode      // The parser mode.
-	TypesConfig     types.Config     // The types configuration.
+	// Context is used if set.
+	Context context.Context
+
+	// Dir is the build system working directory. It defaults to the current one.
+	Dir string
+
+	// Env is the build system environment variables.
+	Env []string
+
+	// Flags is the build system command-line flags.
+	Flags []string
 }
 
-// BuildLoader loads package types from builds.
-var BuildLoader = &Loader{
-	BuildContext:    build.Default,
-	BuildImportMode: build.ImportComment,
-	ParserMode:      parser.ParseComments,
-	TypesConfig:     types.Config{Importer: importer.Default()},
-}
-
-// SourceLoader loads package types from source. It is slower than BuildLoader.
-var SourceLoader = &Loader{
-	BuildContext:    build.Default,
-	BuildImportMode: build.ImportComment,
-	ParserMode:      parser.ParseComments,
-	TypesConfig:     types.Config{Importer: importer.For("source", nil)},
-}
-
-// Load returns the package for the import path.
-func (l *Loader) Load(path string) (*Package, error) {
-	var bp, err = l.BuildContext.Import(path, "", l.BuildImportMode)
+// LoadPackage returns the package for importPath.
+func (l Loader) LoadPackage(importPath string) (*Package, error) {
+	fset := token.NewFileSet()
+	config := &packages.Config{
+		Context:    l.Context,
+		Dir:        l.Dir,
+		Env:        l.Env,
+		BuildFlags: l.Flags,
+		Fset:       fset,
+		Mode:       packages.NeedSyntax,
+	}
+	ps, err := packages.Load(config, importPath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("cannot load package %s: %v", importPath, err)
 	}
-	if len(bp.CgoFiles) == 0 {
-		return l.load(bp, [][]string{bp.GoFiles}, bp.Name)
-	}
-	var c = l.config()
-	c.Import(path)
-	prog, err := c.Load()
-	if err != nil {
-		return nil, err
-	}
-	return l.pkg(bp, prog, prog.Imported[path]), nil
-}
-
-// LoadTest returns the test package for the import path.
-func (l *Loader) LoadTest(path string) (*Package, error) {
-	var bp, err = l.BuildContext.Import(path, "", l.BuildImportMode)
-	if err != nil {
-		return nil, err
-	}
-	if len(bp.CgoFiles) == 0 {
-		return l.load(bp, [][]string{bp.GoFiles, bp.TestGoFiles}, bp.Name)
-	}
-	var c = l.config()
-	c.ImportWithTests(path)
-	prog, err := c.Load()
-	if err != nil {
-		return nil, err
-	}
-	return l.pkg(bp, prog, prog.Imported[path]), nil
-}
-
-// LoadTestExternal returns the external test package for the import path.
-func (l *Loader) LoadTestExternal(path string) (*Package, error) {
-	var bp, err = l.BuildContext.Import(path, "", l.BuildImportMode)
-	if err != nil {
-		return nil, err
-	}
-	if len(bp.XTestGoFiles) == 0 {
+	if len(ps) == 0 {
 		return nil, nil
 	}
-	// TODO: This causes errors like
-	// "/usr/local/Cellar/go/1.10.3/libexec/src/flag/flag_test.go:28:2:
-	// undeclared name: ResetForTesting". The problem seems to be that the
-	// external test package is attempting to use an identifier declared in the
-	// test code of the imported package being used. Since exported declarations
-	// in test code aren't normally importable by other packages, I can
-	// understand this being considered an error. However, lots of standard
-	// library code seems to expect this to work, and indeed it somehow does
-	// when parsed by other code some other way. I can't figure out how to get
-	// it to work. golang.org/x/tools/go/loader only seems to parse
-	// XTestGoFiles, like I do here.
-	//
-	// if len(bp.CgoFiles) == 0 {
-	// 	return l.load(bp, [][]string{bp.XTestGoFiles}, bp.Name+"_test")
-	// }
-	var c = l.config()
-	c.ImportWithTests(path)
-	prog, err := c.Load()
+	return &Package{FileNodes: ps[0].Syntax, FilePositions: fset}, nil
+}
+
+// LoadTestPackage returns the test package for importPath.
+func (l Loader) LoadTestPackage(importPath string) (*Package, error) {
+	fset := token.NewFileSet()
+	config := &packages.Config{
+		Context:    l.Context,
+		Dir:        l.Dir,
+		Env:        l.Env,
+		BuildFlags: l.Flags,
+		Fset:       fset,
+		Tests:      true,
+		Mode:       packages.NeedFiles | packages.NeedName | packages.NeedSyntax,
+	}
+	ps, err := packages.Load(config, importPath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("cannot load package %s: %v", importPath, err)
 	}
-	return l.pkg(bp, prog, prog.Created[0]), nil
-}
-
-func (l *Loader) config() *loader.Config {
-	return &loader.Config{
-		Build:               &l.BuildContext,
-		ParserMode:          l.ParserMode,
-		TypeChecker:         l.TypesConfig,
-		TypeCheckFuncBodies: func(string) bool { return !l.TypesConfig.IgnoreFuncBodies },
-	}
-}
-
-func (l *Loader) load(bp *build.Package, fss [][]string, pkgName string) (*Package, error) {
-	var fset = token.NewFileSet()
-	var lookup = map[string]struct{}{}
-	for _, fs := range fss {
-		for _, f := range fs {
-			lookup[f] = struct{}{}
+	for _, p := range ps {
+		if p.PkgPath == importPath {
+			for _, f := range p.GoFiles {
+				if strings.HasSuffix(f, "_test.go") {
+					return &Package{FileNodes: p.Syntax, FilePositions: fset}, nil
+				}
+			}
 		}
 	}
-	var naps, err = parser.ParseDir(fset, bp.Dir, func(fi os.FileInfo) bool {
-		var _, ok = lookup[fi.Name()]
-		return ok
-	}, l.ParserMode)
-	if err != nil {
-		return nil, err
-	}
-	var ap = naps[pkgName]
-	var fs []*ast.File
-	for _, f := range ap.Files {
-		fs = append(fs, f)
-	}
-	var info = &types.Info{
-		Defs:       map[*ast.Ident]types.Object{},
-		Implicits:  map[ast.Node]types.Object{},
-		Scopes:     map[ast.Node]*types.Scope{},
-		Selections: map[*ast.SelectorExpr]*types.Selection{},
-		Types:      map[ast.Expr]types.TypeAndValue{},
-		Uses:       map[*ast.Ident]types.Object{},
-	}
-	tp, err := l.TypesConfig.Check(bp.ImportPath, fset, fs, info)
-	if err != nil {
-		return nil, err
-	}
-	return &Package{
-		Build:     bp,
-		Nodes:     ap,
-		NodeTypes: info,
-		Tokens:    fset,
-		Types:     tp,
-	}, nil
+	return nil, nil
 }
 
-func (l *Loader) pkg(bp *build.Package, prog *loader.Program, pi *loader.PackageInfo) *Package {
-	var files map[string]*ast.File
-	if len(pi.Files) > 0 {
-		files = map[string]*ast.File{}
-		for _, f := range pi.Files {
-			files[prog.Fset.Position(f.Name.NamePos).Filename] = f
+// LoadExternalTestPackage returns the external test package for importPath.
+func (l Loader) LoadExternalTestPackage(importPath string) (*Package, error) {
+	fset := token.NewFileSet()
+	config := &packages.Config{
+		Context:    l.Context,
+		Dir:        l.Dir,
+		Env:        l.Env,
+		BuildFlags: l.Flags,
+		Fset:       fset,
+		Tests:      true,
+		Mode:       packages.NeedFiles | packages.NeedName | packages.NeedSyntax,
+	}
+	ps, err := packages.Load(config, importPath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot load package %s: %v", importPath, err)
+	}
+	pkgPath := importPath + "_test"
+	for _, p := range ps {
+		if p.PkgPath == pkgPath {
+			return &Package{FileNodes: p.Syntax, FilePositions: fset}, nil
 		}
 	}
-	return &Package{
-		Build: bp,
-		Nodes: &ast.Package{
-			Files: files,
-			Name:  pi.Pkg.Name(),
-		},
-		NodeTypes: &pi.Info,
-		Tokens:    prog.Fset,
-		Types:     pi.Pkg,
-	}
+	return nil, nil
 }
 
-// Package is build, node, token, and type information for a package.
+// Package is file information for a Go package.
 type Package struct {
-	Build     *build.Package // The build.
-	Nodes     *ast.Package   // The nodes.
-	NodeTypes *types.Info    // The node types.
-	Tokens    *token.FileSet // The files.
-	Types     *types.Package // The types.
+	FileNodes     []*ast.File
+	FilePositions *token.FileSet
 }
--- loader_test.go
+++ loader_test.go
@@ -1,209 +1,43 @@
 package turbine
 
-import (
-	"go/ast"
-	"go/format"
-	"os"
-	"sort"
-	"testing"
+import "testing"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/kr/pretty"
-)
+func TestLoader(t *testing.T) {
+	t.Parallel()
 
-var standardExternal = map[string]struct{}{
-	"archive/tar":          struct{}{},
-	"archive/zip":          struct{}{},
-	"bufio":                struct{}{},
-	"bytes":                struct{}{},
-	"compress/flate":       struct{}{},
-	"compress/gzip":        struct{}{},
-	"compress/zlib":        struct{}{},
-	"container/heap":       struct{}{},
-	"container/list":       struct{}{},
-	"container/ring":       struct{}{},
-	"context":              struct{}{},
-	"crypto/cipher":        struct{}{},
-	"crypto/des":           struct{}{},
-	"crypto/md5":           struct{}{},
-	"crypto/rand":          struct{}{},
-	"crypto/sha1":          struct{}{},
-	"crypto/sha256":        struct{}{},
-	"crypto/tls":           struct{}{},
-	"crypto/x509":          struct{}{},
-	"database/sql":         struct{}{},
-	"debug/dwarf":          struct{}{},
-	"encoding/base32":      struct{}{},
-	"encoding/base64":      struct{}{},
-	"encoding/binary":      struct{}{},
-	"encoding/csv":         struct{}{},
-	"encoding/gob":         struct{}{},
-	"encoding/hex":         struct{}{},
-	"encoding/json":        struct{}{},
-	"encoding/pem":         struct{}{},
-	"encoding/xml":         struct{}{},
-	"errors":               struct{}{},
-	"flag":                 struct{}{},
-	"fmt":                  struct{}{},
-	"go/ast":               struct{}{},
-	"go/doc":               struct{}{},
-	"go/parser":            struct{}{},
-	"go/printer":           struct{}{},
-	"go/scanner":           struct{}{},
-	"go/types":             struct{}{},
-	"hash":                 struct{}{},
-	"hash/crc32":           struct{}{},
-	"html":                 struct{}{},
-	"html/template":        struct{}{},
-	"image":                struct{}{},
-	"image/draw":           struct{}{},
-	"image/png":            struct{}{},
-	"index/suffixarray":    struct{}{},
-	"io":                   struct{}{},
-	"io/ioutil":            struct{}{},
-	"log":                  struct{}{},
-	"log/syslog":           struct{}{},
-	"math":                 struct{}{},
-	"math/big":             struct{}{},
-	"math/bits":            struct{}{},
-	"math/cmplx":           struct{}{},
-	"math/rand":            struct{}{},
-	"mime":                 struct{}{},
-	"mime/multipart":       struct{}{},
-	"mime/quotedprintable": struct{}{},
-	"net":                  struct{}{},
-	"net/http":             struct{}{},
-	"net/http/cookiejar":   struct{}{},
-	"net/http/httptest":    struct{}{},
-	"net/http/httptrace":   struct{}{},
-	"net/http/httputil":    struct{}{},
-	"net/mail":             struct{}{},
-	"net/smtp":             struct{}{},
-	"net/url":              struct{}{},
-	"os":                   struct{}{},
-	"os/exec":              struct{}{},
-	"os/signal":            struct{}{},
-	"path":                 struct{}{},
-	"path/filepath":        struct{}{},
-	"reflect":              struct{}{},
-	"regexp":               struct{}{},
-	"runtime":              struct{}{},
-	"runtime/debug":        struct{}{},
-	"runtime/trace":        struct{}{},
-	"sort":                 struct{}{},
-	"strconv":              struct{}{},
-	"strings":              struct{}{},
-	"sync":                 struct{}{},
-	"sync/atomic":          struct{}{},
-	"syscall":              struct{}{},
-	"testing":              struct{}{},
-	"text/scanner":         struct{}{},
-	"text/tabwriter":       struct{}{},
-	"text/template":        struct{}{},
-	"time":                 struct{}{},
-	"unicode":              struct{}{},
-	"unicode/utf16":        struct{}{},
-	"unicode/utf8":         struct{}{},
-}
-
-func TestLoaders(t *testing.T) {
-	var check = func(t *testing.T, path string, p *Package, err error, find bool) {
+	check := func(t *testing.T, p *Package, err error, found bool) {
 		t.Helper()
-		if find {
-			if err != nil {
-				t.Errorf("error: actual %v, expected nil", err)
-			}
+		if err != nil {
+			t.Error("error", err)
+		}
+		if found {
 			if p == nil {
-				t.Errorf("package: actual nil, expected not nil")
+				t.Error("package is nil")
 			} else {
-				if p.Build == nil {
-					t.Errorf("build: actual nil, expected not nil")
+				if p.FileNodes == nil {
+					t.Error("no file nodes")
 				}
-				if p.Nodes == nil {
-					t.Errorf("nodes: actual nil, expected not nil")
-				}
-				if p.NodeTypes == nil {
-					t.Errorf("node types: actual nil, expected not nil")
-				}
-				if p.Tokens == nil {
-					t.Errorf("tokens: actual nil, expected not nil")
-				}
-				if p.Types == nil {
-					t.Errorf("types: actual nil, expected not nil")
+				if p.FilePositions == nil {
+					t.Error("no file positions")
 				}
 			}
 		} else {
-			if err != nil {
-				t.Errorf("error: actual %v, expected nil", err)
-			}
 			if p != nil {
-				t.Errorf("package: actual %#v, expected nil", p)
+				t.Error("found package that should not exist", p)
 			}
 		}
 	}
-	var standardPaths []string
-	for path := range Standard {
-		standardPaths = append(standardPaths, path)
-	}
-	sort.Strings(standardPaths)
-	for _, test := range []struct {
-		name   string
-		loader interface {
-			Load(string) (*Package, error)
-			LoadTest(string) (*Package, error)
-			LoadTestExternal(string) (*Package, error)
-		}
-	}{
-		{"BuildLoader", BuildLoader},
-		{"SourceLoader", SourceLoader},
-	} {
-		t.Run(test.name, func(t *testing.T) {
-			for _, path := range standardPaths {
-				var path = path
-				t.Run(path, func(t *testing.T) {
-					t.Run("Load", func(t *testing.T) {
-						var pkg, err = test.loader.Load(path)
-						check(t, path, pkg, err, true)
-					})
-					t.Run("LoadTest", func(t *testing.T) {
-						var pkg, err = test.loader.LoadTest(path)
-						check(t, path, pkg, err, true)
-					})
-					t.Run("LoadTestExternal", func(t *testing.T) {
-						var pkg, err = test.loader.LoadTestExternal(path)
-						var _, find = standardExternal[path]
-						check(t, path, pkg, err, find)
-					})
-				})
-			}
-		})
-	}
-}
 
-func TestPrint(t *testing.T) {
-	var p, err = BuildLoader.Load("t")
-	if err != nil {
-		t.Fatal(err)
-	}
-	// cm := ast.NewCommentMap(p.Tokens, p.Nodes, p.Nodes.Files["/Users/will/Developer/go/src/t/t.go"].Comments)
-	pretty.Println(p.Nodes)
-	// pretty.Println(cm)
-	// p.Nodes.Files["/Users/will/Developer/go/src/t/t.go"].Comments
+	p, err := LoadPackage("time")
+	check(t, p, err, true)
 
-}
+	p, err = LoadTestPackage("time")
+	check(t, p, err, true)
+	p, err = LoadTestPackage("encoding")
+	check(t, p, err, false)
 
-func TestPrint2(t *testing.T) {
-	var p, err = BuildLoader.Load("t")
-	if err != nil {
-		t.Fatal(err)
-	}
-	var f = p.Nodes.Files["/Users/will/Developer/go/src/t/t.go"]
-	f.Comments[0] = &ast.CommentGroup{List: []*ast.Comment{{Slash: f.Comments[0].List[0].Slash, Text: "// T is u."}}}
-	f.Decls[0].(*ast.FuncDecl).Doc = &ast.CommentGroup{List: []*ast.Comment{{Slash: f.Decls[0].(*ast.FuncDecl).Doc.List[0].Slash, Text: "// T is v."}}}
-	spew.Dump(f.Comments[0])
-	spew.Dump(f.Decls[0].(*ast.FuncDecl).Doc)
-	// pretty.Println(f)
-	if err := format.Node(os.Stdout, p.Tokens, f); err != nil {
-		t.Error(err)
-	}
+	p, err = LoadExternalTestPackage("time")
+	check(t, p, err, true)
+	p, err = LoadExternalTestPackage("encoding")
+	check(t, p, err, false)
 }
