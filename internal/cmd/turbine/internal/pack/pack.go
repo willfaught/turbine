610ce8c5b7e054798be428276b1c441b7de7341c
Will Faught <willfaught@users.noreply.github.com>
2023-07-08 13:59:49 -0700

    Delete templates

--- internal/cmd/turbine/internal/pack/pack.go
+++
@@ -1,6 +0,0 @@
-package __FIELD_Package__
-
-/// {{if false}}
-const __FIELD_Data__ = 0 /// {{end}}
-
-const __FIELD_Identifier__ = __FIELD_Data__
--- internal/cmd/turbine/pack.go
+++
@@ -1,3 +0,0 @@
-package main
-
-const pack = "package __FIELD_Package__\n\n/// {{if false}}\nconst __FIELD_Data__ = 0 /// {{end}}\n\nconst __FIELD_Identifier__ = __FIELD_Data__\n"
